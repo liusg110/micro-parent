e1ac905285b5eca66b5c313167fcfe47412421f5
root <335350168@qq.com>
2018-05-08 15:42:40 +0800

    测试路由网关，负载均衡

--- micro-client1/src/main/java/com/xy/client1/MicroClient1Application.java
+++ micro-client1/src/main/java/com/xy/client1/MicroClient1Application.java
@@ -1,16 +1,14 @@
-package com.xy.gateway;
+package com.xy.client1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient
-@EnableZuulProxy
 @SpringBootApplication
-public class MicroGatewayApplication {
+@EnableEurekaClient
+public class MicroClient1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroGatewayApplication.class, args);
+        SpringApplication.run(MicroClient1Application.class, args);
     }
 }
---
+++ micro-client2/src/main/java/com/xy/client1/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.xy.client1.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title: TestController
+ * @Description: TODO
+ * @author liusg
+ * @date 2018/5/8 15:21
+ * @version V1.0
+ */
+@RestController
+public class TestController {
+
+
+
+    @RequestMapping(value = "api/{test}/{1}")
+    public String test1(@PathVariable(value = "test")String test,@PathVariable(value = "1")String param){
+        System.out.println("---------port------------8763");
+        return "---------port------------8763";
+    }
+}
