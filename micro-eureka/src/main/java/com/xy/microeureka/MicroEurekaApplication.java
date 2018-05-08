622e5f343371ec66b041b2272e8ff0a3c6420758
root <335350168@qq.com>
2018-05-08 11:32:35 +0800

    第一次提交本地git仓库

---
+++ micro-eureka/src/main/java/com/xy/microeureka/MicroEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.xy.microeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroEurekaApplication.class, args);
+    }
+}
