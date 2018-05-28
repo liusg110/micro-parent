91ab4a739a5c731cd483d0bf50696af9f12e716a
刘寿光 <liushouguang@newbeescm.com>
2018-05-28 13:59:38 +0800

    创建workflow微服务，用于工作流的操作

---
+++ micro-workflow/src/main/java/com/xy/workflow/MicroWorkflowApplication.java
@@ -0,0 +1,12 @@
+package com.xy.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroWorkflowApplication.class, args);
+    }
+}
