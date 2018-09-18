1c9a6d991e3086074f5fa4a15c8e205b0eaa6d91
liusg <liushouguang@newbeescm.com>
2018-09-18 16:04:57 +0800

    add new moudule

---
+++ micro-website/src/main/java/com/xiaoyao/website/MicroWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.xiaoyao.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroWebsiteApplication.class, args);
+    }
+}
---
+++ micro-website/src/test/java/com/xiaoyao/website/MicroWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaoyao.website;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroWebsiteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
