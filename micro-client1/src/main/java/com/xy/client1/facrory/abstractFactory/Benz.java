fc920d4aff2c44aa4cb2a5d8b9ab78b80ba45326
liusg <liushouguang@newbeescm.com>
2019-01-02 18:43:00 +0800

    设计模式  抽象工厂模式

---
+++ micro-client1/src/main/java/com/xy/client1/facrory/abstractFactory/Benz.java
@@ -0,0 +1,18 @@
+package com.xy.client1.facrory.abstractFactory;
+/**
+ * @ Name:
+ * @ Author: liusg
+ * @ Date: 2019\1\2 0002 16:44
+ * @ Param:
+ * @ Desc: 奔驰产品的抽象接口
+ */
+public interface Benz {
+    void carColor();  //设置颜色
+
+    void carSpeed(); //设置速度
+
+    void carPrice();  //设置价格
+
+
+
+}
---
+++ micro-client1/src/main/java/com/xy/client1/facrory/abstractFactory/Wheel.java
@@ -0,0 +1,15 @@
+package com.xy.client1.facrory.abstractFactory;
+/**
+ * FileName: Wheel
+ * Author: liusg
+ * Date: 2019\1\2 0002
+ * Desc: 车轱辘产品的抽象接口类
+ *
+ */
+public interface Wheel {
+    void wheelSize();
+
+    void wheelPrice();
+
+
+}
